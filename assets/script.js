a1f17475a1e1407576466e7dcbd6afcf7afbd74e
unknown <berd1495@gmail.com>
2022-02-09 20:09:25 -0600

    updated the funcitonality of the btons

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,90 @@
-// HINT: You can delete this console.log after you no longer need it!
-console.log('JavaScript code has loaded!')
-// First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+// Select some elements...
 
-// We'll use these variables to track the counts of each cookie type
-let gb = 0 // Gingerbread
-let cc = 0 // Chocolate Chip
-let sugar = 0 // Sugar Sprinkle
+console.log('JavaScript loaded!')
 
-// selecting the element with an id of credit
+
+
+// tell your name
+let yourName = "Bryan Rosado" 
+
+// Define Variable 
+let gb = 0      // Gingerbread
+let cc = 0      // Chocolate Chip
+let sugar = 0   // Sugar Sprinkle
+
+// selecting the element 
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
 const gbPlusBtn = document.querySelector('#add-gb')
+const gbMinusBtn = document.querySelector('#minus-gb')
+const ccPlusBtn = document.querySelector('#add-cc')
+const ccMinusBtn = document.querySelector('#minus-cc')
+const sugarPlusBtn = document.querySelector('#add-sugar')
+const sugarMinusBtn = document.querySelector('#minus-sugar')
+const gBDisplay = document.querySelector('#qty-gb')
+const cCDisplay = document.querySelector('#qty-cc')
+const sugarDisplay = document.querySelector('#qty-sugar')
+const Totalqty = document.querySelector('#qty-total')
 
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
-gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+//  "+" button for Gingerbread cookies
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+gbPlusBtn.addEventListener('click', function() {
+    gb++;
+    updateTotal()
+gBDisplay.textContent = gb
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+//  "-" button for Gingerbread cookies
+
+gbMinusBtn.addEventListener('click', function() {
+    if(gb>0) {
+        gb--;
+        updateTotal()
+    gBDisplay.textContent = gb
+    }
+})
+
+//  "+" button for Chocolate Chip
+ccPlusBtn.addEventListener('click', function() {
+    cc++;
+    updateTotal()
+cCDisplay.textContent = cc
+})
+
+//  "-" button for Chocolate Chip
+
+ccMinusBtn.addEventListener('click', function() {
+    if(cc>0) {
+        cc--;
+        updateTotal()
+    cCDisplay.textContent = cc 
+    }
+})
+
+//  "+" button for Sugar
+sugarPlusBtn.addEventListener('click', function() {
+    sugar++;
+    updateTotal()
+sugarDisplay.textContent = sugar
+})
+
+//  "-" button for Sugar
+
+sugarMinusBtn.addEventListener('click', function() {
+    if(sugar>0) {
+        sugar--;
+        updateTotal()
+    sugarDisplay.textContent = sugar
+    }
+})
+
+// Total qty
+
+function updateTotal(){
+  let total = gb+cc+sugar
+  Totalqty.textContent = total
+
+  }
+
